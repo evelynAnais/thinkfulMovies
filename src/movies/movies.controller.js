583cd331852789aa2418d5c9f039134ba4c5ba4c
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-24 15:58:29 -0600

    added get movies and get movies by query

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,2 +1,14 @@
 const service = require('./movies.service');
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+
+async function list(req, res) {
+  if (req.query.is_showing) {
+  res.json({ data:await service.listShowingTrue() }); 
+  } 
+  res.json({ data:await service.list() });
+}
+
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+}
