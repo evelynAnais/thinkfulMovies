ebee0e42bd16c55f2342f934ae1e519e91d87f38
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-25 11:59:28 -0600

    created update review function

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,26 +1,18 @@
+const { where } = require('../db/connection');
 const knex = require('../db/connection');
 const mapProperties = require('../utils/map-properties');
 
 const addCritic = mapProperties({
-  critic_id: 'critic.critic_id',
   preferred_name: 'critic.preferred_name',
   surname: 'critic.surname',
   organization_name: 'critic.organization_name',
-  created_at: 'critic.created_at',
-  updated_at: 'critic.updated_at'
 })
 
 function list(movieId) {
   return knex('reviews')
     .join('critics', 'reviews.critic_id', 'critics.critic_id')
     .select(
-      "reviews.review_id",
-      "reviews.content",
-      "reviews.score",
-      "reviews.created_at",
-      "reviews.updated_at",
-      "reviews.critic_id",
-      "reviews.movie_id",
+      'reviews.*',
       'critics.*'
     )
     .where({ 'reviews.movie_id': movieId })
@@ -31,6 +23,14 @@ function read(review_id) {
   return knex('reviews').select('*').where({ review_id }).first();
 }
 
+function update(updatedReview) {
+  return knex('reviews')
+  .select('*')
+  .where({ review_id: updatedReview.review_id })
+  .update(updatedReview, '*')
+  .then((updatedReviews) => updatedReviews[0])
+}
+
 function destroy(review_id) {
   return knex('reviews').where({ review_id }).del();
 }
@@ -38,5 +38,6 @@ function destroy(review_id) {
 module.exports = {
   list,
   read,
+  update,
   destroy,
 }
