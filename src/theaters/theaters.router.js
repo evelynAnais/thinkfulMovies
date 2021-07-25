1192b1e445015f65cf053264af662bb52bcd1fb9
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-24 20:58:18 -0600

    theater by id function added

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 const controller = require('./theaters.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
 
-// router
-//   .route('/:theatersId')
-//   .all(methodNotAllowed);
+router
+  .route('/:theatersId')
+  .all(methodNotAllowed);
 
 router  
   .route('/')
+  .get(controller.list)
   .all(methodNotAllowed);
 
 module.exports = router;
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,1 +1,9 @@
 const knex = require('../db/connection');
+
+function list() {
+  return knex('theaters').select('*');
+}
+
+module.exports = {
+  list,
+}
