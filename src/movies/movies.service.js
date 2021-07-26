a2051ffe4c2dd422fd94535280e7b72a89bad825
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-26 14:26:59 -0600

    fixed movies serice selection to display on front

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -4,12 +4,22 @@ function list() {
   return knex('movies').select('*');
 }
 
+// function listShowingTrue() {
+//   return knex('movies_theaters')
+//     .join('movies', 'movies_theaters.movie_id', 'movies.movie_id')
+//     .select('*') 
+//     .where({ 'is_showing': true } )
+//     .groupBy('movies.title');
+//     //.orderBy('movies_theaters.movie_id');
+// }
+
 function listShowingTrue() {
-  return knex('movies_theaters')
-    .join('movies', 'movies_theaters.movie_id', 'movies.movie_id')
-    .select('*') 
+  return knex('movies')
+    .join('movies_theaters', 'movies.movie_id', 'movies_theaters.movie_id')
+    .select('movies.*') 
     .where({ 'is_showing': true } )
-    .groupBy('movies.title');
+    .groupBy('movies.title', 'movies.movie_id');
+    //.orderBy('movies_theaters.movie_id');
 }
 
 function read(movie_id) {
